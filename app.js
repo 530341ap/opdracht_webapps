384a1b63a346abf32545032f8947a23fe3cdef62
Aïssa Peters <aissa.peters@gmail.com>
2017-11-24 12:10:29 +0100

    Folderstructuur bijwerken voor Heroku deployment

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./server/routes/index');
+var users = require('./server/routes/users');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/recipedb', {  useMongoClient: true });
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const port = process.env.PORT || '3000';
+app.set('port',port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
